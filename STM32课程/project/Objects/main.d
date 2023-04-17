75c905c01ba34755a0cb51cdd11330e2211e1580
magician <blackstone10086@gmail.com>
2023-04-17 19:00:22 +0800

    nothing

---
+++ STM32课程/project/Objects/main.d
@@ -0,0 +1,2 @@
+.\objects\main.o: main.c
+.\objects\main.o: D:\file_test\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ STM32课程/project/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("sdfhfsghjfgj\n");
+	
+	return 0;
+
+}
